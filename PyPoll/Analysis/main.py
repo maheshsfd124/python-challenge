074a5ecfc6d03dfc21959b441f34b3384e721f8e
maheshsfd124 <mahesh.sfd@gmail.com>
2021-03-31 13:18:31 -0400

    added vote count

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -14,19 +14,48 @@ with open(election_csv,newline ='', encoding='utf-8') as electionfile:
     for row in election_input:
         election_votes.append(row[0])
         election_candidate.append(row[2])
+        total = len(election_votes)
     print(" Total number of votes casted : " + " " + str(len(election_votes)))
     print(" Total number of canidate index: " + " " + str(len(election_candidate)))
-    print (election_candidate[2])
+    # print (election_candidate[2])
 ind =[]
 
 # finding index to get index of amount list
-for count in election_votes:
-    ind.append(election_votes.index(count))
-print(ind)
-# res ={}
-# res = dict(zip(election_candidate,ind))
-# print (str(res))
+count1 =0 
+count2 =0
+count3 =0
+count4 =0
+for x in election_candidate:
+    if x == "Khan":
+        count1 = count1 + 1
+        count1per = count1/total *100
+    elif x == "Correy":
+        count2 = count2 + 1
+        count2per = count2/total *100
+    elif x == "Li":
+        count3 = count3 + 1
+        count3per = count3/total *100
+    elif x == "O'Tooley":
+        count4= count4 + 1
+        count4per = count4/total *100
 
+print(count1)
+print(count2)
+print(count3)
+print(count4)
+print (count1per)
+print (count2per)
+print (count3per)
+print (count4per)
+
+print("Election Results")
+print("------------------------")
+print ("Total Votes : " + " " + str(len(election_votes)))
+print("------------------------")
+print("Khan: " + " " + str(count1per) + "%" +" (" + str(count1) +")")
+print("Correy: " + " " + str(count2per) + "%" +" (" + str(count2) +")")
+print("Li: " + " " + str(count3per) + "%" +" (" + str(count3) +")")
+print("O'Tooley: " + " " + str(count4per) + "%" +" (" + str(count4) +")")
          
  
  
